7a3506ee93f51076e25311e8fbe6dc0eb5cf0cff
launico-ralph-andrei <dreilaunico@gmail.com>
2025-02-17 15:49:03 +0800

    Add files via upload

---
+++ DICTIONARY CODES.py
@@ -0,0 +1,33 @@
+DATA = {
+"Abacus":"Around 4000 years ago, the Chinese invented the Abacus, and it is believed to be the first computer. The history of computers begins with the birth of the abacus. Structure: Abacus is basically a wooden rack that has metal rods with beads mounted on them.Working of abacus: In the abacus, the beads were moved by the abacus operator according to some rules to perform arithmetic calculations. In some countries like China, Russia, and Japan, the abacus is still used by their people.",
+"Napier's Bones":"Was a manually operated calculating device and as the name indicates, it was invented by JohnNapier. In this device, he used 9 different ivory strips (bones) marked with numbers to multiply and divide for calculation. It was also the first machine to use the decimal point system for calculation.",
+"Cracker" : "Person/people that will try to steal your information/data.",
+"Volatile" : "The contents are lost forever when computer is destroyed, turned off.",
+"RAM" : "this acronym stands for Random Access Memory, this is an electronic device that is much faster than a hard disk drive.",
+"Leibniz Wheel" : "it is a digital mechanical calculator, also known as a Stepped Reckoner.",
+"Tabulating Machine" : "this machine was used by U.S. Census in the year 1890, it is based on punch cards and is capable of tabulating statistics and record or sort data or information.",
+"Differential Analyzer" : "this was the first electronic computer introduced in the year 1930 in the U.S., and this is an analog device that consists of vacuum tubes to switch electrical signals to perform calculations.",
+"Digital Computers" : "modern computers that are capable of processing information in discrete form.",
+"Super Computers" : "these are the fastest and very expensive computers.",
+"Analog Computers" : "these computers are used to process data by ongoing physical processes. (i.e., thermometers, simulators, etc.)",
+"CPU" : "this acronym stands for Central Processing Unit. This is a hardware component of a computer, as it acts as the brain of the computer.",
+"Hybrid Computers" : "this computer specifically uses both analog and digital technology.",
+"Micro Computers" : "a type of digital computer that uses a microprocesor.",
+"Personal Computers" : "called PC in short, these are computers that are affordable that are designed for individual users, commonly used for entertainment, personal productivity and communication.",
+"Computer": "this is an electronic device that can receive, store, process, and output data. It is a machine that can perform a variety of tasks and operations, ranging from simple calculations to complex simulations and artificial intelligence.",
+"Mark I": "Mark I computer was built as a partnership between IBM-and-Harvard. It was also the first programmable digital computer marking a new era in the computer world.",
+"First Generation Computers" : "these machines are slow, huge, and expensive. In this generation of computers, vacuum tubes were used as the basic components of CPU and memory. Also, they were mainly dependent on the batch operating systems and punch cards) Magnetic tape and paper tape were used as output and input devices.",
+"Mainframe Computers" : "These are large and powerful computers that are used by large organizations such as banks, airlines, and government agencies to process massive amounts of data and handle multiple users simultaneously.",
+"Hackers" : "They will just warn you something about malicious activity going around in your computer. They will not steal your information.",
+"Embedded System" : "these are specialized computers that are built into other devices such as cars, appliances, and medical equipment to control their operations and perform specific functions.",
+"System Software" : "these are softwares, without it in our PC/laptop, it won't run. It is must for a device to be operating. (i.e.: Linux, Unix, Windows, etc.",
+"Mobile Device/s" : "these are small and portable computers that are designed for on-the-go use, such as smartphones, tablets, and laptops.",
+"Application Software/s" : "these are those software, even without in the system of our PC/laptop, it can still function normally and it can run, as these are software/s that are not necessary for a device to be operating. (i.e.: Facebook, Tiktok, Mobile games, etc.",
+"Multitasking" : "this is one of the main advantages of computers as it can do multiple tasks.",
+"Memory" : "measure of computer storage in terms of Bytes, Kilobytes, Megabytes, Gigabytes, etc.",
+"Malware and viruses" : "this is a common issue in computers, as it can infect it and cause a wide range of problems. (i.e., Data Loss, System Crashes and Identity Theft.",
+"Compatibility Issues" : "software applications/hardware components may not be compatible with each other, leading to system crashes or display problems.",
+"Security" : "this is one of the main advantages of a computer, as it is possible to use security measures with the help of which computers are protected and do not allow any malicious program and other similar threats to access the materials."
+}
+a = input("seacch: ")
+print(DATA[a])
